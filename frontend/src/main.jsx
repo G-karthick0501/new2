b63c4b2a14d2a59dcd223789fe68c23a6a06ec7a
G-karthick0501 <karthick.venger@gmail.com>
2025-10-31 14:03:35 +0530

    tried changing ui but it did not wrk so have commented out the index.css in main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+// in src/main.jsx
+//import './index.css';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
